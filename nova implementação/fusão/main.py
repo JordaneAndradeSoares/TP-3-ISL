573438ee7bacda148ab473ad1649bb7b95f58567
JordaneAndradeSoares <113549535+JordaneAndradeSoares@users.noreply.github.com>
2026-06-18 16:59:57 -0300

    Add files via upload

--- nova implementação/fusão/main.py
+++ nova implementação/fusão/main.py
@@ -1,16 +1,28 @@
+import os
+
 from extra_alfabeto import ler_afd_com_alfabeto # importando função da implementação da AF
-from ap import AutomatoDePilha
+from ap import AutomatoDePilha #importando classe da implementação da AP
     
 # função principal para chamar as funções de processamento dos arquivos
 if __name__ == "__main__":
-    print("SIMULADOR MULTI-AUTOMATOS UNIFICADO\nFormatos de simulador suportados: @AF, @MT, @ALL e @AP")
+    print("SIMULADOR MULTI-AUTOMATOS UNIFICADO\nFormatos de simulador suportados: @AF, @MT, @ALL e @AP\n")
 
-    print("digite o arquivo de entrada:")
-    arquivoEntrada = input()
+    # pedindo o nome do arquivo de entrada para o usuário
+    arquivoEntrada = input("Digite o arquivo de entrada: ")
+    print(f"\nProcessando o arquivo '{arquivoEntrada}'...\nResultados:")
 
-    with open('nova implementação/fusão/' + arquivoEntrada, 'r', encoding='utf-8') as arquivo:
+    # criando o caminho do arquivo de entrada   
+    caminho = os.path.join('arquivos', arquivoEntrada)
 
-        entrada = arquivo.read()
+    # tentando abrir o arquivo de entrada
+    try:
+        with open(caminho, 'r', encoding='utf-8') as arquivo:
+            entrada = arquivo.read()
+
+    # tratando o erro caso não consiga abrir o arquivo de entrada
+    except FileNotFoundError:
+        print(f"\nErro: arquivo '{caminho}' não encontrado.")
+        exit()
 
     vetLinhas = entrada.split('\n')
     tipoAutomato = vetLinhas.pop(0)
@@ -22,4 +34,8 @@ if __name__ == "__main__":
         case "@AP":
             AutomatoDePilha.leituraPilha(vetLinhas)
 
-        #fazer o resto dos cases para as outras maquinas
+        case "@MT":
+            print("Simulador de Máquina de Turing AINDA não implementado.") #TODO: fazer essa maquina
+
+        case "@ALL":
+            print("Simulador de Máquina de Turing AINDA não implementado.") #TODO: fazer essa maquina
