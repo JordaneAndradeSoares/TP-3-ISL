076fc49df34ffd26c73a87a4ebd8c3c4443a7212
Vini_amm <vinicius.ammiranda@gmail.com>
2026-06-21 10:39:35 -0300

    Add files via upload
    Implementa simulador de maquina de Turing

--- nova implementação/fusão/main.py
+++ nova implementação/fusão/main.py
@@ -3,6 +3,7 @@ import os
 from extra_alfabeto import ler_afd_com_alfabeto # importando função da implementação da AF
 from ap import AutomatoDePilha #importando classe da implementação da AP
 from afn import leituraAFN #importando classe da implementaçao do AFN
+from mt import leituraMT #importando classe da implementaçao do MT
 
 # função principal para chamar as funções de processamento dos arquivos
 if __name__ == "__main__":
@@ -39,7 +40,7 @@ if __name__ == "__main__":
             leituraAFN(vetLinhas)
 
         case "@MT":
-            print("Simulador de Máquina de Turing AINDA não implementado.") #TODO: fazer essa maquina
+             leituraMT(vetLinhas)
 
         case "@ALL":
-            print("Simulador de Máquina de Turing AINDA não implementado.") #TODO: fazer essa maquin
+           print("Simulador de Máquina de Turing AINDA não implementado.") #TODO: fazer essa maquin
